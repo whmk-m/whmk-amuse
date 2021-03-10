e8af35dd414675ea479dc7e98417ebca5bf6aa25
wangmao <1454435379@qq.com>
2021-03-10 22:07:22 +0800

    添加sass、并声明主题色配置变量

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
-import './App.css';
+import './App.scss';
+import {ReactElement} from "react";
 
-function App() {
+function App(): ReactElement {
+  console.log('aaa')
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
